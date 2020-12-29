9875b47aeb65bc92201d5c9a4551420445a53e44
Fillipi Nascimento <fillipi-nascimento@hotmail.com>
2020-12-29 16:52:28 -0300

    feat: create appointment

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { isEqual } from 'date-fns'
+import Appointment from '../models/Appointment'
+
+class AppointmentsRespository {
+  private appointments: Appointment[]
+
+  constructor() {
+    this.appointments = []
+  }
+
+  public findAll(): Appointment[] {
+    return this.appointments
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    )
+
+    return findAppointment || null
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date)
+
+    this.appointments.push(appointment)
+
+    return appointment
+  }
+}
+
+export default AppointmentsRespository
---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+
+import appointmentsRoute from './appointments.route'
+
+const routes = Router()
+
+routes.use('/appointments', appointmentsRoute)
+
+export default routes
